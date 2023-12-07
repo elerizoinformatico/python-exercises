a9f2b177948b0415030706ffc79813e192b0282d
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-06 18:19:34 -0600

    Create exercise-1.py

---
+++ section-1-introduction/exercise-1.py
@@ -0,0 +1,8 @@
+# Identifica el tipo de dato en los siguientes valores
+
+a = "Hola mundo" # String
+b = [1, 10, 100] # Lista
+c = -25 # Int
+d = 1.167 # Float
+e = ["Hola", "Mundo"] # Lista
+f = ' ' # String
