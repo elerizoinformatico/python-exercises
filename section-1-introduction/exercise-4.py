dbcb26acc951bdfdcf251351fddfee9b5b3d6d33
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-07 09:32:07 -0600

    Create exercise-4.py

---
+++ section-1-introduction/exercise-4.py
@@ -0,0 +1,13 @@
+# Al igual que en el ejercicio anterior queremos obtener la nota media.
+# El problema es que cada nota tiene un valor porcentual:
+# La primera nota vale un 15% del total
+# La segunda nota vale un 35% del total
+# La tercera nota vale un 50% del total
+# Debes desarrollar un programa para calcular perfectamente la notal final.
+
+nota_1 = 10
+nota_2 = 7
+nota_3 = 4
+
+media = (nota_1 * 0.15) + (nota_2 * 0.35) + (nota_3 * 0.5)
+print("La nota media es: ", media)
