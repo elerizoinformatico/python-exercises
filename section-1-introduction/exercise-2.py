2d0d33a75c0cb0e621fa184515c64f4961e28959
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-06 18:25:57 -0600

    Create exercise-2.py

---
+++ section-1-introduction/exercise-2.py
@@ -0,0 +1,14 @@
+# Determina mentalmente el resultado que aparecerá por pantalla
+
+a = 10
+b = -5
+c = "Hola "
+d = [1, 2, 3]
+
+print(a * 5) # 50
+print(a - b) # 15
+print(c + "Mundo") # "Hola Mundo"
+print(c * 2) # "Hola Hola "
+print(c[-1]) # " "
+print(c[1:]) # "ola "
+print(d + d) # [1, 2, 3, 1, 2, 3]
