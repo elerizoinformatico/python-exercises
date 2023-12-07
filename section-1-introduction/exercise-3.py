9db280dd1f19a6da89f138d8a44216bdb9744261
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-07 09:24:34 -0600

    Create exercise-3.py

---
+++ section-1-introduction/exercise-3.py
@@ -0,0 +1,9 @@
+# El siguiente código pretende realizar una media entre 3 números pero no funciona.
+# Detectar el error y solucionarlo.
+
+numero_1 = 9
+numero_2 = 3
+numero_3 = 6
+
+media = numero_1 + numero_2 + numero_3 / 3
+print("La nota media es: ", media)
