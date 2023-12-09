661f131ed094b04e1e1b74e3905248a3ccad989b
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-08 18:32:40 -0600

    Update 6-nested-loops.py

--- section-3-flow-control/6-nested-loops.py
+++ section-3-flow-control/6-nested-loops.py
@@ -1,1 +1,19 @@
-print("Hola")
+"""
+El objetivo del ejercicio es modificar el contenido de la matriz dinámicamente,
+sustituyendo todos sus números pares por 0 y los impares por 1.
+"""
+
+matriz = [
+    [8,  7,  0],
+    [34, 2, -1],
+    [5, -5, 12]
+]
+
+for i, fila in enumerate(matriz):
+    for j, columna in enumerate(fila):
+        if matriz[i][j] % 2 == 0:
+            matriz[i][j] = 0
+        else:
+            matriz[i][j] = 1
+
+print(matriz)
