7530df7c9b2a1ed603c7e9303ce9bcfd80016d70
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-11 20:53:30 -0600

    Update 1-tuples.py

--- section-4-data-collections/1-tuples.py
+++ section-4-data-collections/1-tuples.py
@@ -11,9 +11,20 @@ Nota: En total son 6 prints uno debajo de otro, no puedes redefinir ni modificar
 el test fallará, simplemente muestra lo que se pide por pantalla.
 """
 
+# Último elemento de la tupla
 print(tupla[-1])
+
+# Número de elementos que tiene la tupla
 print(len(tupla))
+
+# Posición donde se encuentra el número 123 de la tupla
 print(tupla.index(123))
+
+# Una lista con los primeros tres elementos de la tupla
 print(list(tupla[:3]))
+
+# Elemento que hay en la posición con índice 4 de la tupla
 print(tupla[4])
+
+# Número de veces que aparece el número 4 en la tupla
 print(tupla.count(4))
