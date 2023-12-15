4ea0052474845e99877aeeb2a64aed5939fc8ed7
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-15 13:52:00 -0600

    Update exercise-1.py

--- section-4-data-collections/exercise-1.py
+++ section-4-data-collections/exercise-1.py
@@ -4,7 +4,7 @@ Realiza un programa que siga las siguientes instrucciones:
 - Crea un conjunto llamado administradores con los administradores Juan y Marta
 - Borra al administador Juan del conjunto de administradores
 - Añade a Marcos como un nuevo administrador, pero no lo borres del conjunto de usuarios
-- Muestra todos los usuarios por pantalla de forma dinámica, indicando si cada usuario es administrador o no
+- Muestra todos los usuarios de forma dinámica, indicando si cada usuario es administrador o no
 
 Nota: Los conjuntos se pueden recorrer dinámicamente utilizando un bucle for de forma similar a una lista.
 También cuentan con un método llamado discard(elemento) que sirve para borrar un elemento
