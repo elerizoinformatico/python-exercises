3a695bb2fa5e7819bcb40b136f612b013dd67383
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-18 16:15:12 -0600

    Create exercise-7.py

---
+++ section-6-functions/exercise-7.py
@@ -0,0 +1,20 @@
+"""
+Realiza una función factores_primos() que reciba un número entero y despliegue sus factores primos.
+
+Por ejemplo:
+factores_primos(10) [2,5]
+factores_primos(20) [2,2,5]
+"""
+
+def factores_primos(numero):
+    factores = []
+    divisor = 2
+    while divisor <= numero:
+        if numero % divisor == 0:
+            factores.append(divisor)
+            numero = numero // divisor
+        else:
+            divisor += 1
+    return factores
+
+print(factores_primos(20))
