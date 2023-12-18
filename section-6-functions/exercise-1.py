92d1b91ed3916861b2d24972adf3f0f7373ca064
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-18 15:34:31 -0600

    Create exercise-1.py

---
+++ section-6-functions/exercise-1.py
@@ -0,0 +1,9 @@
+"""
+Realiza una función llamada area_rectangulo() que devuelva el área a partir de una base y una altura.
+Prueba la función para un rectángulo de 15 de base y 10 de altura.
+"""
+
+def area_rectangulo(b,h):
+    return b*h
+
+print(area_rectangulo(15,10))
