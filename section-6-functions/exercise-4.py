9bc599f54bbe63ccbbcb2b451a9f764e0a3d8b03
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-18 15:48:45 -0600

    Create exercise-4.py

---
+++ section-6-functions/exercise-4.py
@@ -0,0 +1,9 @@
+"""
+Realiza una función llamada intermedio() que a partir de dos números, devuelva su punto intermedio.
+Comprueba el punto intermedio entre -12 y 24.
+"""
+
+def intermedio(a,b):
+    return (a+b)/2
+
+print(intermedio(-12,24))
