e98c8b9da1db088ba83dfc2cba4a5d51701738f0
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-19 12:55:48 -0600

    Update exercise-1.py

--- section-7-exceptions/exercise-1.py
+++ section-7-exceptions/exercise-1.py
@@ -6,3 +6,10 @@ programa se bloquee y además explica en un mensaje al usuario la causa y/o solu
 resultado = 10/0
 # ZeroDivisionError
 # ZeroDivisionError: division by zero
+
+# Añadiendo un try-except tenemos
+try:
+    resultado = 10/0
+    print(resultado)
+except ZeroDivisionError:
+    print("¡Error! No es posible dividir por cero")
