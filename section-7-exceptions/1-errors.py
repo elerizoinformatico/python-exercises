8b1694d0b29d8e516e0653cb86cfae99979fd583
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-19 10:57:59 -0600

    Create 1-errors.py

---
+++ section-7-exceptions/1-errors.py
@@ -0,0 +1,6 @@
+"""
+En la función del ejercicio hay un fallo potencial, averigua cuando sucede y retorna el valor None en ese caso especial,
+en cualquier otro caso devuelve el resultado normal de la función.
+
+Pista: Valor indeterminado.
+"""
