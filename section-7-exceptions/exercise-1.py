6ac655522b0be5a09cdd2811b69ce168296b4830
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-19 12:51:53 -0600

    Create exercise-1.py

---
+++ section-7-exceptions/exercise-1.py
@@ -0,0 +1,8 @@
+"""
+Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el
+programa se bloquee y además explica en un mensaje al usuario la causa y/o solución.
+"""
+
+resultado = 10/0
+# ZeroDivisionError
+# ZeroDivisionError: division by zero
