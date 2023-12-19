756a0c8a8042b8aa5e869f83c46144b80de12fb2
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-19 14:02:03 -0600

    Update exercise-3.py

--- section-7-exceptions/exercise-3.py
+++ section-7-exceptions/exercise-3.py
@@ -5,3 +5,10 @@ programa se bloquee y además explica en un mensaje al usuario la causa y/o solu
 
 colores = {"rojo": "red", "verde": "green", "negro": "black"}
 print(colores["blanco"]) # KeyError: 'blanco'
+
+# Añadimos la sentencia try-except
+try:
+    print(colores["blanco"])
+except KeyError:
+    print("¡Error! La clave del diccionario no se encuentra")
+    print("Prueba con alguna que exista: {rojo, verde, negro}")
