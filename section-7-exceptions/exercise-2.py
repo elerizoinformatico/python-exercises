ff415c897dc39b5e26c747bae6a39f4e76820ce9
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-19 13:57:09 -0600

    Update exercise-2.py

--- section-7-exceptions/exercise-2.py
+++ section-7-exceptions/exercise-2.py
@@ -5,3 +5,10 @@ programa se bloquee y además explica en un mensaje al usuario la causa y/o solu
 
 lista = [1, 2, 3, 4, 5]
 print(lista[10]) # IndexError: list index out of range
+
+# Añadimos la sentencia try-except
+try:
+    print(lista[10])
+except IndexError:
+    print("¡Error! El índice al que intentas acceder se encuentra fuera del rango")
+    print("Ejemplo: lista[0-4]")
