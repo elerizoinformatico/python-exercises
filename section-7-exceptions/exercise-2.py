034d2bb3d953a767120893d63278ccef5471e66a
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-19 13:53:38 -0600

    Create exercise-2.py

---
+++ section-7-exceptions/exercise-2.py
@@ -0,0 +1,7 @@
+"""
+Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el
+programa se bloquee y además explica en un mensaje al usuario la causa y/o solución.
+"""
+
+lista = [1, 2, 3, 4, 5]
+print(lista[10]) # IndexError: list index out of range
