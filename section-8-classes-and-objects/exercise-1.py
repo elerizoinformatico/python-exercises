6d24837b80d255a971c842ead123a8f397858fa0
Nicolás Soto Soto <nisoto4004@gmail.com>
2023-12-26 15:28:56 -0600

    Update and rename exercice-1.py to exercise-1.py

---
+++ section-8-classes-and-objects/exercise-1.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio:
+- Crea una clase llamada Punto con sus dos coordenadas X e Y.
+- Añade un método constructor para crear puntos fácilmente. Si no se recibe una coordenada su valor será 0.
+- Sobreescribe el método string para mostrar un punto en formato (X,Y).
+- Añade un método cuadrante que indique a qué cuadrante pertenece el punto o si es el origen.
+- Añade un método vector que tome otro punto y calcule el vector resultante entre los dos puntos.
+- Añade un método distancia que tome otro punto y calcule la distancia entre los dos puntos y la muestre por pantalla.
+"""
