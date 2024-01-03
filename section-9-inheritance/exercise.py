8ceb1b7cf2e430a8c49b9180dfc34fcbb0e8b043
Nicolás Soto Soto <nisoto4004@gmail.com>
2024-01-02 23:54:36 -0600

    Create exercise.py

---
+++ section-9-inheritance/exercise.py
@@ -0,0 +1,21 @@
+"""
+A partir del ejemplo anterior, extiende la clase Vehiculo y realiza la siguiente implementación:
+
+Vehiculo:
+- Color
+- Ruedas
+
+Coche(Vehiculo):
+- Velocidad (km/h)
+- Cilindrada (cc)
+
+Bicicleta(Vehiculo):
+- Tipo (urbana/deportiva)
+
+Camioneta(Coche):
+- Carga (kg)
+
+Motocicleta(Bicicleta):
+- Velocidad (km/h)
+- Cilindrada (cc)
+"""
