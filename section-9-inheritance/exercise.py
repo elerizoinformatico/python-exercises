4d977059fe6c85d2aecf8352ff7f01b6757baa6d
Nicolás Soto Soto <nisoto4004@gmail.com>
2024-01-03 00:00:14 -0600

    Update exercise.py

--- section-9-inheritance/exercise.py
+++ section-9-inheritance/exercise.py
@@ -18,4 +18,11 @@ Camioneta(Coche):
 Motocicleta(Bicicleta):
 - Velocidad (km/h)
 - Cilindrada (cc)
+
+Experimenta:
+- Crea al menos un objeto de cada subclase y añádelos a una lista llamada vehiculos.
+- Realiza una función llamda catalogar() que reciba la lista de vehiculos y muestre el nombre de su clase y sus atributos.
+- Modifica la función catalogar() para que reciba un argumento optativo ruedas, haciendo que muestre únicamente los que su
+  número de ruedas concuerde con el valor del argumento. También debe mostrar un mensaje "Se han encontrado {} vehiculos con
+  {} ruedas" únicamente si se envía el argumento ruedas. Ponla a prueba con 0, 2 y 4 ruedas como valor.
 """
